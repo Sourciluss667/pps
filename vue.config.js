8a31c4f98b0caf789b60fd1a0555c693b51039da
Sourciluss667 <qcormand@sourciluss.net>
2020-03-09 22:00:02 +0100

    .

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+// vue.config.js
+module.exports = {
+    // options...
+    devServer: {
+        // open: process.platform === 'darwin',
+        host: '0.0.0.0',
+        port: 420 // CHANGE YOUR PORT HERE!
+        // https: false,
+        // hotOnly: false,
+      }
+  }
+  
